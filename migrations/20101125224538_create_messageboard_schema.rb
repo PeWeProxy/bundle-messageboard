a1d6af0d80a12edcfa89b60a76b3849938c708b4
Tomas Kramar <kramar.tomas@gmail.com>
2010-11-25 22:52:03 +0100

    Fix migrations

--- migrations/20101125224538_create_messageboard_schema.rb
+++ migrations/20101125224538_create_messageboard_schema.rb
@@ -1,21 +1,21 @@
-class CreateSchema < ActiveRecord::Migration
+class CreateMessageboardSchema < ActiveRecord::Migration
   def self.up
     create_table :messageboard_messages do |t|
       t.integer :id, :limit => 11
       t.string :userid, :limit => 32
       t.timestamp :datetime
-      t.string, :url, :limit => 4000
+      t.string :url, :limit => 4000
       t.text :text
     end
 
-	add_index :messageboard_messages, :url, [:url, :userid]
+    add_index :messageboard_messages, [:url, :userid]
 
     create_table :messageboard_user_preferences do |t|
       t.integer :id, :limit => 11	
       t.string :userid, :limit => 32
       t.string :messageboard_nick, :limit => 12
       t.string :language, :limit => 2
-      t.boolean visibility
+      t.boolean :visibility
     end
 
     add_index :messageboard_user_preferences, :userid
