34f816f57e37cb6b09cbf6193ced8a66c3e36b52
Ivan Plesko <ivan.plesko@gmail.com>
2010-11-28 16:24:38 +0100

    Add delete and edit functionality to keywords

--- static/peweproxy_keywords.js
+++ static/peweproxy_keywords.js
@@ -1,21 +1,21 @@
 var temp = function($) {
-    $(document).ready(function(){
-        $('div#peweproxy_icon_banner a.__peweproxy_keywords_button').click(function(){
-            $(this).blur();
-            renewSmallButton = false;
-            $('#peweproxy_keywords').hide().removeClass('hidden').fadeIn('fast');
-            $(peweproxy_addonIconBannerSelector).addClass('hidden');
-            $(smallButtonSelector).addClass('hidden');
-            peweproxy_get_keywords();
-            return false;
-        });
-        $('div#peweproxy_keywords a.__peweproxy_keywords_closebutton').click(function(){
-            $(this).blur();
-            renewSmallButton = true;
-            $(smallButtonSelector).removeClass('hidden');
-            $('#peweproxy_keywords').fadeOut('fast');
-            return false;
-        });
+	$(document).ready(function(){
+		$('div#peweproxy_icon_banner a.__peweproxy_keywords_button').click(function(){
+			$(this).blur();
+			renewSmallButton = false;
+			$('#peweproxy_keywords').hide().removeClass('hidden').fadeIn('fast');
+			$(peweproxy_addonIconBannerSelector).addClass('hidden');
+			$(smallButtonSelector).addClass('hidden');
+			peweproxy_get_keywords();
+			return false;
+		});
+		$('div#peweproxy_keywords a.__peweproxy_keywords_closebutton').click(function(){
+			$(this).blur();
+			renewSmallButton = true;
+			$(smallButtonSelector).removeClass('hidden');
+			$('#peweproxy_keywords').fadeOut('fast');
+			return false;
+		});
 	});
 } (adaptiveProxyJQuery);
 
@@ -26,25 +26,57 @@ function peweproxy_keywords_edit(id){
 	} (adaptiveProxyJQuery);
 }
 
+function peweproxy_keywords_delete(id){
+	var temp = function($) {
+		if (confirm('Skutočne odstrániť kľúčové slovo '+$('div#peweproxy_keywords_content table tr.row'+id+' span.term').html()+'?')){
+			$.post(peweproxy_url+'?action=removeKeyWord',{
+				id:id
+			},function(data){
+				response = $.trim(data);
+				if (response == 'OK'){
+					peweproxy_get_keywords();
+				} else {
+					alert('Pri mazaní kľúčového slova nastala chyba.');
+				}
+			});
+		}
+		} (adaptiveProxyJQuery);
+}
+
 function peweproxy_keywords_save(id){
 	var temp = function($) {
-		$('div#peweproxy_keywords_content table tr.row'+id+' span.editable').hide();
-		$('div#peweproxy_keywords_content table tr.row'+id+' span.static').show();
+		table = $('div#peweproxy_keywords_content table');
+		post_data = {
+			id:id,
+			term:table.find("tr.row"+id+" input.term").val(),
+			relevance: table.find("tr.row"+id+" input.relevance").val(),
+			type: table.find("tr.row"+id+" input.type").val()
+		}
+		$.post(peweproxy_url+'?action=editKeyWord',post_data, function(data){
+			response = $.trim(data);
+			if (response == 'OK'){
+				peweproxy_get_keywords()
+			} else if (response == 'TERM_EXISTS'){
+				alert('Kľúčové slovo '+post_data.term+' už existuje');
+			} else {
+				alert('Pri ukladaní kľúčového slova nastala chyba');
+			}
+		});
 	} (adaptiveProxyJQuery);
 }
 
 function peweproxy_get_keywords(){
-	var template = '<tr class="row[:id:]">'+
+	var template = '<tr class="row[:id:] keyword_row">'+
 						'<td style="width: 179px">'+
-							'<span class="static">[:term:]</span>'+
+							'<span class="static term">[:term:]</span>'+
 							'<span class="editable"><input type="text" class="term" value="[:term:]"/></span>'+
 						'</td>'+
 						'<td style="width: 56px">'+
-							'<span class="static">[:type:]</span>'+
+							'<span class="static type">[:type:]</span>'+
 							'<span class="editable"><input type="text" class="type" value="[:type:]"/></span>'+
 						'</td>'+
 						'<td style="width: 43px">'+
-							'<span class="static">[:relevance:]</span>'+
+							'<span class="static relevance">[:relevance:]</span>'+
 							'<span class="editable"><input type="text" class="relevance" value="[:relevance:]"/></span>'+
 						'</td>'+
 						'<td style="width: 57px">[:source:]</td>'+
@@ -62,6 +94,7 @@ function peweproxy_get_keywords(){
 		$.post(peweproxy_url+'?action=getKeyWords','data=1',function(data){
 			keywords = eval('('+data+')');
 			table = $('div#peweproxy_keywords_content table');
+			table.find("tr.keyword_row").remove();
 			for (var keyword in keywords.keywords){
 				row = template.replace(/\[:id:\]/g, keywords.keywords[keyword].id);
 				row = row.replace(/\[:term:\]/g, keywords.keywords[keyword].term);
@@ -70,6 +103,7 @@ function peweproxy_get_keywords(){
 				row = row.replace(/\[:source:\]/g, keywords.keywords[keyword].source);
 				table.append(row);
 			}
+			$('div#peweproxy_keywords_content table tr:even').addClass('odd');
 		})
 	} (adaptiveProxyJQuery);
 }
