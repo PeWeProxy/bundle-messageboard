a87d18a0ff9ebd17eee61872878d2c27e9503bc8
Jan Hudek <hudek.jan@gmail.com>
2010-12-15 20:05:32 +0100

    Add debug information to keyword: response

--- src/sk/fiit/rabbit/adaptiveproxy/plugins/services/messageboard/KeyWordsProcessingPlugin.java
+++ src/sk/fiit/rabbit/adaptiveproxy/plugins/services/messageboard/KeyWordsProcessingPlugin.java
@@ -174,9 +174,9 @@ public class KeyWordsProcessingPlugin  extends JavaScriptInjectingProcessingPlug
 		String formatedTimeStamp = timestamp.substring(0, timestamp.indexOf("."));
 		
 		try {
-			if (Float.parseFloat(relevance) > 1) return "FAIL";
+			if (Float.parseFloat(relevance) > 1) return "FAIL " + " floate.parseFloat exception";
 		} catch (Exception e) {
-			 return "FAIL";
+			 return "FAIL " + e;
 		}
 		
 		try {
@@ -189,7 +189,7 @@ public class KeyWordsProcessingPlugin  extends JavaScriptInjectingProcessingPlug
 				pageId = rs.getInt(1);
 			}
 			
-			if (pageId == -1) return "FAIL";
+			if (pageId == -1) return "FAIL " + " pageid -1 exception";
 			
 			stmt = connection.prepareStatement("SELECT `pages_terms`.`id`, `pages_terms`.`active` FROM `pages_terms` JOIN `terms` ON `pages_terms`.`term_id` = `terms`.`id` WHERE `pages_terms`.`page_id` = ? AND `terms`.`label` = ?;");
 			stmt.setInt(1, pageId);
@@ -225,7 +225,7 @@ public class KeyWordsProcessingPlugin  extends JavaScriptInjectingProcessingPlug
 			return "OK";
 		} catch (SQLException e) {
 			logger.error("Could not add key word ", e);
-			return "FAIL";
+			return "FAIL " + e;
 		} finally {
 			SqlUtils.close(stmt);
 		}
